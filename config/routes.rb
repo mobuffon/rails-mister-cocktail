3d5ad294d922276fb38406d55149a9aa889eab76
Moritz von Buchwaldt <moritzvonbuchwaldt@web.de>
2019-11-07 14:22:26 +0100

    basic cocktail features done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # get 'cocktails/index'
+  # get 'cocktails/show'
+  # get 'cocktails/create'
+  # get 'cocktails/new'
+  resources :cocktails, only: [:new, :show, :create, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
