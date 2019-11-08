bb0e482705bef5404126e9e1ab92f4b0211bc9b7
Moritz von Buchwaldt <moritzvonbuchwaldt@web.de>
2019-11-08 15:24:06 +0100

    addet env and gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-
-  resources :cocktails, only: [:new, :show, :create, :index, :destroy] do
+  root to: 'cocktails#index
+  '
+  resources :cocktails, only: [ :index, :new, :show, :create, :destroy] do
     resources :doses, only: [:new, :create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
